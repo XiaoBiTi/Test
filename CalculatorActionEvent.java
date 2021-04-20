856a6eedbb6a297630d4f76df7373b22ecfdd2bd
小鼻涕孩 <82190196+XiaoBiTi@users.noreply.github.com>
2021-04-20 09:14:13 +0800

    Add files via upload

--- CalculatorActionEvent.java
+++ CalculatorActionEvent.java
@@ -2,12 +2,11 @@ import javax.swing.*;
 import java.util.Arrays;
 
 public class CalculatorActionEvent {
-    private boolean first = true;
-    private long result = 0;
+    private double result = 0;
     private JButton button;
     private JLabel label;
     private JLabel label1;
-    private String temp;
+    private String temp = "";
     public CalculatorActionEvent(){
         System.out.println("欢迎使用");
     }
@@ -20,6 +19,8 @@ public class CalculatorActionEvent {
     public void ButtonAction() {
         String[] number = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."};
         Arrays.sort(number);
+		
+		
         if (Arrays.binarySearch(number, button.getText()) > 0) {
             if (label1.getText() == "0") {
                 label1.setText(button.getText());
@@ -27,27 +28,79 @@ public class CalculatorActionEvent {
                 label1.setText(label1.getText() + button.getText());
             }
         } else {
-            if (first) {
-                temp = button.getText();
+            if (temp.equals("") && button.getText() != "C" && button.getText() != "CE" && button.getText() != "=" && button.getText() != "Backspace") {
+                temp = button.getText().toString();
                 label.setText(label1.getText() + button.getText());
-                result = Long.parseLong(label1.getText());
+                result = Double.parseDouble(label1.getText());
+				System.out.println(label1.getText());
                 label1.setText("0");
-                first = false;
             } else {
                 switch (temp) {
                     case "+":
-                        result += Long.parseLong(label1.getText());
+                        result += Double.parseDouble(label1.getText());
                         break;
                     case "×":
-                        result *= Long.parseLong(label1.getText());
+                        result *= Double.parseDouble(label1.getText());
+						System.out.println(label1.getText());
                         break;
-                    case "":
+					case "-":
+                        result -= Double.parseDouble(label1.getText());
+						
                         break;
+                    case "+/-":
+                        result = -Double.parseDouble(label1.getText());
+						
+                        break;
+					case "÷":
+                        result /= Double.parseDouble(label1.getText());
+						
+                        break;
+                    case "x^0.5":
+                        result = Math.pow(Double.parseDouble(label1.getText()),0.5);
+
+                        break;
+                    case "x^2":
+						result = Math.pow(Double.parseDouble(label1.getText()),2);
+						
+                        break;
+					case "1/x":
+						result = 1/Double.parseDouble(label1.getText());
+						
+                        break;
+					case "%":
+						result %= Double.parseDouble(label1.getText());
+						break;	
+					default:
+						if(button.getText() == "=" && temp != ""){
+							label.setText(label.getText() + label1.getText());
+							label1.setText(String.valueOf(result));
+							
+							result = 0;
+							temp = "=";
+						}else{
+					
+							if(button.getText() == "CE"){
+								label.setText("");
+								label1.setText("0");
+								result = 0; 
+								temp = "CE";
+							}else if(button.getText()  == "C"){
+								label1.setText("0");
+								temp = "C";
+							}else if(button.getText()  == "Backspace"){
+								label1.setText(label1.getText().substring(0,label1.getText().length()-2));
+								temp = "Backspace";
+							}else{
+								temp = button.getText();
+								label.setText(String.valueOf(result));
+								label1.setText("0");
+							}
+						}
+					
 
                 }
-                temp = button.getText();
-                label.setText(String.valueOf(result));
-                label1.setText("0");
+				
+                
             }
         }
 
